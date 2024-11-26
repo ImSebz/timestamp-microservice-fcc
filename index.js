b04eba2b701c2eb5e20f99608ba9436db4349142
Sebastián Camargo <juansecapri@hotmail.com>
2024-11-26 04:37:30 +0000

    timestamp solution

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 // where your node app starts
 
 // init project
@@ -18,10 +17,34 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const isInvalidDate = (date) => date.toUTCString() === "Invalid Date";
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if (isInvalidDate(date)) {
+    res.json({
+      error: "Invalid Date"
+    });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+// /api Empty
+app.get("/api", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
